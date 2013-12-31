533b50d712461bfb5cd9aa82bfb6a5f3e4dbccd0
dmcb <d.mcburney@gmail.com>
2013-12-31 12:11:26 -0700

    Default Cocos 2D app.

---
+++ Barbary Coast/AppDelegate.h
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.h
+//  Barbary Coast
+//
+//  Created by Derek McBurney on 12/31/2013.
+//  Copyright Derek McBurney 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "cocos2d.h"
+
+// Added only for iOS 6 support
+@interface MyNavigationController : UINavigationController <CCDirectorDelegate>
+@end
+
+@interface AppController : NSObject <UIApplicationDelegate>
+{
+	UIWindow *window_;
+	MyNavigationController *navController_;
+	
+	CCDirectorIOS	*director_;							// weak ref
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (readonly) MyNavigationController *navController;
+@property (readonly) CCDirectorIOS *director;
+
+@end
---
+++ Barbary Coast/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Barbary Coast
+//
+//  Created by Derek McBurney on 12/31/2013.
+//  Copyright Derek McBurney 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
